bd3d99cdb6a0b3c40d27e8bca7df1f9855dc4940
Geovany Rico <geo981202@gmail.com>
2023-01-10 21:30:41 -0500

    Pagina products empezada

--- app.js
+++ app.js
@@ -7,12 +7,21 @@ const PORT = 3001
 
 app.use(express.static('src/public'))
 
-app.get('/home', (req , res) => {
-    res.sendFile(path.resolve(__dirname,'src/views/index.html'))
+app.get('/home', (_ , res) => {
+    res.sendFile(path.resolve('src/views/home.html'))
 })
 
+app.get('/comprar', (_, res) => {
+    res.sendFile(path.resolve('src/views/forms.html'))
+})
 
+app.get('/product', (_, res) => {
+    res.sendFile(path.resolve('src/views/product.html'))
+})
 
+app.get('/products', (_, res) => {
+    res.sendFile(path.resolve('src/views/products.html'))
+})
 
 app.listen(PORT,()=>{
     console.log(`Server on port ${PORT}`)
