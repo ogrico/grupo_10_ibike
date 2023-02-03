92142ead8823e57e2c0ec5db434e262515e809f3
Andrés Verjhman Home <anfeuver@gmail.com>
2023-02-02 21:49:03 -0500

    Error git add .!

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,44 @@
+const dataProducts = [
+    {
+        des: "bici ruta 1",
+        price: "$10000",
+        img: "img/products/bikes/bike2.jpg"
+    },
+    {
+        des: "bici ruta 2",
+        price: "$20000",
+        img: "img/products/bikes/bike3.jpg"
+    },
+    {
+        des: "bici ruta 3",
+        price: "$30000",
+        img: "img/products/bikes/bike2.jpg"
+    },
+    {
+        des: "bici ruta 4",
+        price: "$40000",
+        img: "img/products/bikes/bike3.jpg"
+    }
+]
+
+
+
+let products = {
+
+    getProducts: (_, res) => {
+
+/*         res.render('products', {
+            dataProducts
+        }) */
+        res.sendFile(path.resolve('src/views/list.html'))
+
+    }
+
+}
+
+
+
+
+
+
+module.exports = products
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,36 +1,11 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const path = require('path')
+const productsController = require('../controllers/products.controller')
+
 
 const router = Router()
 
-const dataProducts = [
-    {
-        des:"bici ruta 1",
-        price:"$10000",
-        img:"img/products/bikes/bike2.jpg"
-    },
-    {
-        des:"bici ruta 2",
-        price:"$20000",
-        img:"img/products/bikes/bike3.jpg"
-    },
-    {
-        des:"bici ruta 3",
-        price:"$30000",
-        img:"img/products/bikes/bike2.jpg"
-    },
-    {
-        des:"bici ruta 4",
-        price:"$40000",
-        img:"img/products/bikes/bike3.jpg"
-    }
-]
 
-router.get('/products', (_, res) => {
-    res.render('products', {
-        dataProducts
-    })
-    //res.sendFile(path.resolve('src/views/list.html'))
-})
+router.get('/products',productsController.getProducts())
 
 module.exports = router
