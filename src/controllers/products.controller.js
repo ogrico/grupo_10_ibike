b73c606c825f59f0fd088b62d8b606424760d33b
Geovany Rico <geo981202@gmail.com>
2023-02-06 19:27:21 -0500

    views ajustadas

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,44 +1,12 @@
-const dataProducts = [
-    {
-        des: "bici ruta 1",
-        price: "$10000",
-        img: "img/products/bikes/bike2.jpg"
-    },
-    {
-        des: "bici ruta 2",
-        price: "$20000",
-        img: "img/products/bikes/bike3.jpg"
-    },
-    {
-        des: "bici ruta 3",
-        price: "$30000",
-        img: "img/products/bikes/bike2.jpg"
-    },
-    {
-        des: "bici ruta 4",
-        price: "$40000",
-        img: "img/products/bikes/bike3.jpg"
-    }
-]
-
-
-
-let products = {
+var products = {
 
     getProducts: (_, res) => {
 
-/*         res.render('products', {
-            dataProducts
-        }) */
+        //res.render('product')
         res.sendFile(path.resolve('src/views/list.html'))
 
     }
 
 }
-
-
-
-
-
 
 module.exports = products
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,8 @@
 const { Router } = require('express')
-const path = require('path')
 const productsController = require('../controllers/products.controller')
-
 
 const router = Router()
 
-
-router.get('/products',productsController.getProducts())
+router.get('/products', productsController.getProducts)
 
 module.exports = router
