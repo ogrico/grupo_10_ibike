b72d85844e8dd440632cf4877f977759264eea56
Geovany Rico <geo981202@gmail.com>
2023-02-16 07:18:23 -0500

    Profile.html terminado

--- src/public/js/profile.js
+++ src/public/js/profile.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-    console.log('Ready...')    
+    console.log('Ready...')
+    
+    
+    $('#dp').click(function(){
+        $('#personales').css('display', 'block')
+        $('#facturacion').css('display', 'none')
+        $('#pedidos').css('display', 'none')
+    })
+
+    $('#df').click(function(){
+        $('#personales').css('display', 'none')
+        $('#facturacion').css('display', 'block')
+        $('#pedidos').css('display', 'none')
+    })
+
+    $('#hp').click(function(){
+        $('#personales').css('display', 'none')
+        $('#facturacion').css('display', 'none')
+        $('#pedidos').css('display', 'block')
+    })
+    
 })
