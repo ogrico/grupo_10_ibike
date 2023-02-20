9ca881df4c4a3560d6c498de9c155c2668ea08a8
Geovany Rico <geo981202@gmail.com>
2023-02-19 22:48:31 -0500

    Metodo Update terminado

--- src/controllers/crud.controller.js
+++ src/controllers/crud.controller.js
@@ -1,15 +1,15 @@
 const fs = require("fs"),
   path = require("path"),
-  bikesDataPath = path.join(__dirname, "../data/bikes.json");
+  bikesDataPath = path.join(__dirname, "../data/bikes.json")
 
-let bikes = JSON.parse(fs.readFileSync(bikesDataPath, "utf-8"));
+let bikes = JSON.parse(fs.readFileSync(bikesDataPath, "utf-8"))
 
 const curd = {
   formProduct: (_, res) => {
-    res.render("create");
+    res.render("create")
   },
   createProduct: (req, res) => {
-    console.log(req.file);
+    console.log(req.file)
 
     let newBody = {
       imagen1: req.files[0].originalname,
@@ -37,89 +37,74 @@ const curd = {
           descripcion: req.body.esp_descripcion2,
         },
       ],
-    };
+    }
 
-    bikes.push(newBody);
-    console.log("Bicicleta a crear : ", newBody, "\n");
+    bikes.push(newBody)
+    console.log("Bicicleta a crear : ", newBody, "\n")
 
     try {
-      fs.writeFileSync(bikesDataPath, JSON.stringify(bikes));
-      console.log("Todo Ok");
+      fs.writeFileSync(bikesDataPath, JSON.stringify(bikes))
+      console.log("Todo Ok")
       res.render("products", {
         bikes,
-      });
+      })
     } catch (error) {
-      console.log("error : " + error);
-      res.render("create");
+      console.log("error : " + error)
+      res.render("create")
     }
   },
   formUpdateProduct: (req, res) => {
-    let bike = bikes.filter((bike) => bike.referencia == req.params.id);
-    res.render("put", { bike });
+    let bike = bikes.filter((bike) => bike.id == req.params.id)
+    res.render("put", { bike })
   },
   updateProduct: (req, res) => {
-    let bike = bikes.filter(bike => bike.referencia == req.params.id)
+
+    console.log(req.body)
+
+    let temp = bikes.filter(bike => bike.id == req.params.id),
+      bike = {
+        "id": temp[0].id,
+        "destacado": temp[0].destacado,
+        "imagen1": temp[0].imagen1,
+        "imagen2": temp[0].imagen2,
+        "nombre": req.body.nombre,
+        "tipo": req.body.tipo,
+        "modelo": req.body.modelo,
+        "referencia": req.body.referencia,
+        "valor": req.body.valor,
+        "descuento": req.body.descuento,
+        "talla": req.body.talla,
+        "descripcion": req.body.descripcion,
+        "especificaciones": [
+          {
+            "nombre": req.body.esp_nombre,
+            "descripcion": req.body.esp_descripcion3
+          },
+          {
+            "nombre": req.body.esp_nombre2,
+            "descripcion": req.body.esp_descripcion2
+          },
+          {
+            "nombre": req.body.esp_nombre3,
+            "descripcion": req.body.esp_descripcion3
+          }
+        ]
+      },
+      newBikes = bikes.filter(bike => bike.id != req.params.id)
+
+    newBikes.push(bike)
+
     try {
-        let newBody = {
-            imagen1: req.files[0].originalname,
-            imagen2: req.files[1].originalname,
-            id: 100,
-            nombre: req.body.nombre,
-            tipo: req.body.tipo,
-            modelo: req.body.modelo,
-            referencia: req.body.referencia,
-            valor: req.body.valor,
-            descuento: req.body.descuento,
-            talla: req.body.talla,
-            descripcion: req.body.descripcion,
-            especificaciones: [
-              {
-                nombre: req.body.esp_nombre,
-                descripcion: req.body.esp_descripcion,
-              },
-              {
-                nombre: req.body.esp_nombre2,
-                descripcion: req.body.esp_descripcion2,
-              },
-              {
-                nombre: req.body.esp_nombre2,
-                descripcion: req.body.esp_descripcion2,
-              },
-            ],
-          };   
-           
-            
-            bike[0].nombre = req.body.nombre,
-            bike[0].tipo = req.body.tipo,
-            bike[0].modelo = req.body.modelo,
-            bike[0].referencia = req.body.referencia,
-            bike[0].valor = req.body.valor,
-            bike[0].descuento = req.body.descuento,
-            bike[0].talla = req.body.talla,
-            bike[0].descripcion = req.body.descripcion,
-         
-            
-            bike[0].descripcion[0].nombre = req.body.esp_nombre,
-            bike[0].descripcion[0].descripcion = req.body.esp_descripcion,
-              
-            bike[0].descripcion[1].nombre = req.body.esp_nombre2,
-            bike[0].descripcion[1].descripcion = req.body.esp_descripcion2,
-              
-            bike[0].descripcion[2].nombre = req.body.esp_nombre2,
-            bike[0].descripcion[2].descripcion = req.body.esp_descripcion2
-              
-            
-        
+      fs.writeFileSync(bikesDataPath, JSON.stringify(newBikes))
+      console.log("Todo Ok", '\n', bike)
+      let bikesDes = bikes.filter(bike => bike.destacado === true)
+      res.render('index', { bikesDes })
     } catch (error) {
-        
+      console.log(error)
+      res.render('products', { bikes })
     }
-    
 
-    bikes.push(newBody);
-    console.log("Bicicleta a actualizar : ", newBody, "\n");
-
-   
   },
-};
+}
 
-module.exports = curd;
+module.exports = curd
---
+++ src/middlewares/updateBikeValidator.js
@@ -0,0 +1,50 @@
+const { check, validationResult } = require('express-validator'),
+    fs = require('fs'),
+    path = require('path'), bikesDataPath = path.join(__dirname, "../data/bikes.json")
+
+let bikes = JSON.parse(fs.readFileSync(bikesDataPath, "utf-8"))
+
+const updateBikeValidator = [
+    check('nombre').notEmpty().withMessage('Nombre vacio'),
+    check('tipo').notEmpty().withMessage('Tipo vacio'),
+    check('modelo').notEmpty().withMessage('Modelo vacio').isNumeric().withMessage('Debe ser un valor numerico').custom(value => {
+
+        let año = new Date()
+        if (value > año.getFullYear()) {
+            throw new Error('Modelo no valido')
+        }
+        return true
+    }),
+    check('referencia').notEmpty().withMessage('Referencia vacio').isNumeric().withMessage('Debe ser un valor numerico'),
+    check('valor').notEmpty().withMessage('Valor vacio').isNumeric().withMessage('Debe ser un valor numerico'),
+    check('descuento').notEmpty().withMessage('Descuento vacio'),
+    check('descripcion').notEmpty().withMessage('Nombre vacio'),
+    check('esp_nombre').notEmpty().withMessage('Tipo vacio'),
+    check('esp_descripcion').notEmpty().withMessage('Nombre vacio'),
+    check('esp_nombre2').notEmpty().withMessage('Tipo vacio'),
+    check('esp_descripcion2').notEmpty().withMessage('Tipo vacio'),
+    check('esp_nombre3').notEmpty().withMessage('Tipo vacio'),
+    check('esp_descripcion3').notEmpty().withMessage('Tipo vacio'),
+    (req, res, next) => {
+
+        let body = req.body
+        let errors = validationResult(req)
+
+        console.log(req.body, '\n')
+
+        if (!errors.isEmpty()) {
+            console.log('Errores : ', errors.mapped(), '\n')
+            let bike = bikes.filter((bike) => bike.id == req.params.id)
+            res.render('put', {
+                errors: errors.mapped(),
+                oldBody: body,
+                bike
+            })
+        } else {
+            console.log('Next()')
+            next()
+        }
+    }
+]
+
+module.exports = updateBikeValidator
