b965d3045f3ee2e9ae10aa98416edad0ba89048a
Geovany Rico <geo981202@gmail.com>
2023-05-24 15:27:45 -0500

    Ajuste multer create product

--- src/app/middlewares/multer/bikeImgMulter.js
+++ src/app/middlewares/multer/bikeImgMulter.js
@@ -2,7 +2,7 @@ const multer = require('multer'),
     path = require('path')
 
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, '../../public/img/products/bikes'),
+    destination: path.join(__dirname, '../../public/img/products'),
     filename: (_, file, cb) => {
         cb(null, file.originalname)
     }
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage,
-    dest: path.join(__dirname, '../../public/img/products/bikes')
+    dest: path.join(__dirname, '../../public/img/products')
 }).array('imagenes')
 
 module.exports = upload
